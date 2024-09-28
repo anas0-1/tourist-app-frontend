b52f47dd93450c840a218e507910dd7708e53635
anas0-1 <anaszrigui01@gmail.com>
2024-09-29 00:08:57 +0100

    nav bar on the dashboard

--- src/views/PageDashboard.vue
+++ src/views/PageDashboard.vue
@@ -2,7 +2,7 @@
   <div class="flex h-screen bg-blue-50">
     <DashboardSidebar />
     <div class="flex-1 flex flex-col overflow-hidden">
-      <TopBar :title="currentRouteName" /> <!-- Pass the current route name as title -->
+      <NavBar />
       <main class="flex-1 overflow-y-auto bg-blue-50 p-6">
         <!-- Dynamically load child components  -->
         <router-view />
@@ -12,11 +12,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
 import DashboardSidebar from '@/components/Dashboard/DashboardSidebar.vue'
-import TopBar from '@/components/Dashboard/TopBar.vue'
+import NavBar from '@/components/tools/NavBar.vue'
 
-const route = useRoute()
-const currentRouteName = computed(() => route.name || 'Dashboard') 
 </script>
